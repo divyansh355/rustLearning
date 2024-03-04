7623008c8be40f4fcf42193f9152fcddd45e0dc6
divyansh <devmehta4369@outlook.com>
2024-03-04 20:24:00 +0530

    learnt -> variables, constants, shadowing, mutability

---
+++ r2_variables/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    // Mutable Variables
+    // let mut x = 11;
+    // println!("The value of x is: {x}");
+    // x = 22;
+    // println!("The value of x is: {x}");
+
+    // Constants
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Value of constant: {THREE_HOURS_IN_SECONDS} ");
+
+    // Shadowing
+    // let x = 5;
+    // let x = x + 1;
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
+    // println!("The value of x is: {x}");
+
+    // Shadowing
+    // let spaces = "   ";
+    // let spaces = spaces.len();
+
+    // Shadowing
+    // let mut spaces = "   ";
+    // spaces = spaces.len();
+}
